be8860c64355d2a2747ad3bb5be570fcb7f4542a
Goliaten <ognikat4@gmail.com>
2026-03-05 00:46:17 +0100

    fix for logs not being saved out of docker

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
+import os
+
 LOGGER_NAME_PREFIX = "main"
 LOG_LEVEL = "DEBUG"
-LOG_DIR = "logs"
+LOG_DIR = os.path.join("src", "logs")
 ALL_METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"]
